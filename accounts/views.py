3c318a1193ec047e3935bfc4264992399ebdcdc0
erhic <eric.gichovi@student.moringaschool.com>
2022-03-05 14:05:55 +0300

    setting up my view functions

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+
+def login(request):
+    return HttpResponse ('this is is the begining')
+    
+def register():
+    pass
