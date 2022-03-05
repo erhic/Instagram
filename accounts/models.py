054296a0bcea28eb6e6b8988b188f4fe4e5de89f
erhic <eric.gichovi@student.moringaschool.com>
2022-03-06 01:49:33 +0300

    added models classes and made updation

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,27 @@ from django.db import models
 
 # Create your models here.
 class Images(models.Model):
-    pass
+    user=models.ForeignKey('User',on_delete=models.CASCADE,blank=True)
+    image= models.ImageField()
+    image_name= models.CharField(max_length=50)
+    image_caption= models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.image
+    
+    
 class Likes(models.Model):
-    pass
+    user=models.ForeignKey('Users',on_delete=models.CASCADE,blank=True)
+    images= models.ForeignKey('Images',on_delete=models.CASCADE,blank=True)
+    likes= models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.likes
+
 class Comments(models.Model):
-    pass
+    user=models.ForeignKey('Users',on_delete=models.CASCADE,blank=True)
+    images= models.ForeignKey('Images',on_delete=models.CASCADE,blank=True)
+    comments= models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.comments
