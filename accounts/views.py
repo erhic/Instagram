e69bbd605de749a144ca095b11ba22576d894ef7
erhic <eric.gichovi@student.moringaschool.com>
2022-03-05 22:32:53 +0300

    added a function,to render the templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
+import re
 from django.shortcuts import render
-from django.http.response import HttpResponse
+# from django.http.response import HttpResponse
 
 # Create your views here.
 
 def login(request):
-    return HttpResponse ('this is is the begining')
+    return render(request,'accounts/navbar.html')
     
 def register():
     pass
