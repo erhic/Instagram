2bcad8f3dc38543efaedd648cdd6852d20c52df9
erhic <eric.gichovi@student.moringaschool.com>
2022-03-06 23:22:01 +0300

    set the urlpatterns path for static media url and root

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
 urlpatterns=[
-    path('post/',views.login,name='post'),
+    path('home/',views.index,name='home'),
     path('', views.login_user, name='login'),
     path('register/', views.register, name='register')
     
     ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
