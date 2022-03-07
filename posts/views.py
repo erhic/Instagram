4c3ccd498105a2df204f50b4a44e7608274f6e37
erhic <eric.gichovi@student.moringaschool.com>
2022-03-08 00:17:36 +0300

    set up function ,to render post template

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
+from multiprocessing import context
+from re import template
 from django.shortcuts import render
 
+from .models import Post
+
+from django.views.generic import (ListView)
+
 # Create your views here.
+
+class PostListView(ListView):
+    template_name= 'posts/post.html'
+    queryset= Post.objects.all()
+    context_object_name='posts'
+
