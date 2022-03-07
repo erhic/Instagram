cf0b7b394bce5d82daeaaa0bf315c8d4d2dc4f2b
erhic <eric.gichovi@student.moringaschool.com>
2022-03-07 08:30:40 +0300

    added email console configuration settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -143,6 +143,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 # Default primary key field type
