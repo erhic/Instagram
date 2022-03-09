25a5b2a498f16220cd7daafe5856ae9728606fa3
erhic <eric.gichovi@student.moringaschool.com>
2022-03-09 16:17:04 +0300

    updated url path

--- instagram/urls.py
+++ instagram/urls.py
@@ -23,10 +23,11 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    path('',auth_views.LoginView.as_view(template_name='usersaccount/login.html'),name='login'),
     path('admin/', admin.site.urls),
     path('register/',user_views.register,name='register'),
     path('profile/',user_views.profile,name='profile'),
-    path('login/',auth_views.LoginView.as_view(template_name='usersaccount/login.html'),name='login'),
+   
     path('logout/',auth_views.LogoutView.as_view(template_name='usersaccount/logout.html'),name='logout'),
     # path('',include('accounts.urls')),
     path('',include('posts.urls')),
